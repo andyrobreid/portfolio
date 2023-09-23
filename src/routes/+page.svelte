e9633a7921d48e6a0636bfde9dfe199599f3173b
Andrew Reid <andyrobreid@gmail.com>
2023-09-23 23:19:56 +0100

    Added links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Portfolio</h1>
-<p>Andrew Reid</p>
+
+
+<title>Portfolio</title>
+<h1>Andrew Reid</h1>
+
+<p><a href="https://github.com/andyrobreid">Github</a></p>
+<p><a href="https://www.linkedin.com/in/andrew-reid-879450189">Linked-in</a></p>
+
+<p>Currently in an Electrical engineering post at <a href="https://mallaghangse.com/">Mallaghan Group</a> developing and maintaining mobile PLC system software.</p>
