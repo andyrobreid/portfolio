ee6fcf2be68605a1bfee97c669bbf281ac91c674
Andrew Reid <andrewreid.reid1@gmail.com>
2024-01-22 22:29:02 +0000

    changed from github.dev

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,3 +4,4 @@
     $title = 'About'
 </script>
 <p>Currently in an Electrical engineering post at <a href="https://mallaghangse.com/">Mallaghan Group</a> developing and maintaining mobile PLC system software.</p>
+<p>Testing this change out on github.dev</p>
