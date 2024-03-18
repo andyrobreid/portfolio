51c3bd05a0e4822463e6ff418272ed4d0d1c8f2a
Andrew Reid <andyrobreid@gmail.com>
2024-03-18 18:25:38 +0000

    Updated home page welcome

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
-	import Links from "$lib/Links.svelte";
     import title from "$lib/title";
 	
     $title = 'Home'
 </script>
 
-<p>Welcome to the first website I have created and deployed on the web.</p>
-<p>I aim to use this to learn web development, showcase my intrests and possibly if recognised, my skills.</p>
+<p>Welcome to my first public website.</p>
+<p>Used to learn web development, showcase interests and share projects.</p>
+<p>- Andrew.</p>
+
+
