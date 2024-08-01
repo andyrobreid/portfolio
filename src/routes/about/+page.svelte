69cae11b9104025d8ed237c857684755a16c95d9
andyrobreid <andyrobreid@gmail.com>
2024-08-01 13:38:27 +0000

    Change from mobile

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-	import title from "$lib/title";
+    import title from "$lib/title";
 
     $title = 'About'
 </script>
 <p>Currently in an Electrical engineering post at <a href="https://mallaghangse.com/">Mallaghan Group</a> developing and maintaining mobile PLC system software.</p>
 <p>Testing this change out on github.dev</p>
+<p>Used <a href="https://github.com/CypherpunkArmory/UserLAnd">UserLand</a> to develop on mobile. (experimental)</p>
