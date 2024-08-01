9854cc1342ae19e3d5873d82d24cf7c05ead1162
andyrobreid <andyrobreid@gmail.com>
2024-08-01 19:33:16 +0000

    fixed comma

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -3,7 +3,7 @@ export async function load({params}){
 	//TODO implement list of blogs hard coded
 	return {
 		result: [
-			{id:1: path:"1-Lets_Start.md"}
+			{id:1, path:"1-Lets_Start.md"}
 		]
 	}
 }
