bfa81c697c699ffabfc4e7b1a891c088f427af0c
andyrobreid <andyrobreid@gmail.com>
2024-08-02 20:53:16 +0000

    test hardcoded fix

--- src/routes/blog/[id]/+page.server.js
+++ src/routes/blog/[id]/+page.server.js
@@ -3,7 +3,7 @@ import {files} from "$lib/blog.js";
 
 export async function load({ params }){
 	const item = files[parseInt(params.id)]
-	let response = await fetch("/1-Lets_Start.md!)
+	let response = await fetch("/1-Lets_Start.md")
 	console.log(response)
 	return {
 		id: item.id,
