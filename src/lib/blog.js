75e62e13a674292bc666a87811552268f788e508
andyrobreid <andyrobreid@gmail.com>
2024-08-02 22:07:00 +0000

    added function to get correct blog

--- src/lib/blog.js
+++ src/lib/blog.js
@@ -2,5 +2,11 @@ export const files = [
 	{id:1, path:"1-Lets_Start.md"}
 ]
 
+export function GetFileFromParam(param){
+	const i = parseInt(param) - 1;
+	return files[i];
+}
+
+//TODO function for correctly labeling blog posts for blog titles sake
 
 
