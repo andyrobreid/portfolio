c28123b7e2e6ba0536dfd4569388ba20fcdbaba5
andyrobreid <andyrobreid@gmail.com>
2024-08-02 14:12:29 +0000

    button click debug trigger

--- src/lib/Tester.svelte
+++ src/lib/Tester.svelte
@@ -1,12 +1,15 @@
 <script>
 //TODO implement component to allow for testing on dev branch, especially on mobile. To view data etc. Maybe react to a keystroke etc
 	export let data;
+	function clickHandler(){
+		console.log(data)
+	}
 </script>
 
 <div class="tester">
 	<p>{JSON.stringify(data)}</p>
 </div>
-
+<button on:click={clickHandler()}>Trigger</button>
 <style>
 	.tester {
 		background: blue;
