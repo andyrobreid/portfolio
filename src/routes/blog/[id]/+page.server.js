a07d5ca875b3fdb533db4d4783ff16adedc118b0
andyrobreid <andyrobreid@gmail.com>
2024-08-02 21:38:41 +0000

    set index

--- src/routes/blog/[id]/+page.server.js
+++ src/routes/blog/[id]/+page.server.js
@@ -2,9 +2,8 @@
 import {files} from "$lib/blog.js";
 
 export async function load({ params }){
-	const item = files[1]
-	let response = await fetch("https://portfolio-git-dev-andyrobreids-projects.vercel.app/1-Lets_Start.md")
-	console.log(response)
+	const item = files[0];
+	let response = await fetch("https://portfolio-git-dev-andyrobreids-projects.vercel.app/1-Lets_Start.md");
 	return {
 		id: item.id,
 		path: item.path,
