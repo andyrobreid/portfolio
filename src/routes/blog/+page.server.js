8d697f7abc6a023a604485ab4bf82f1e0488831d
andyrobreid <andyrobreid@gmail.com>
2024-08-02 14:36:08 +0000

    use blog.js

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -1,9 +1,7 @@
-
+import files from "$lib/blog.js";
 export async function load({params}){
 	//TODO implement list of blogs hard coded
 	return {
-		result: [
-			{id:1, path:"1-Lets_Start.md"}
-		]
+		result: files	
 	}
 }
