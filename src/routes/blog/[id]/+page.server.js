21ba8164e314aa533dccf8fe44d7409362600eeb
andyrobreid <andyrobreid@gmail.com>
2024-08-02 22:11:36 +0000

    corrected post fetch

--- src/routes/blog/[id]/+page.server.js
+++ src/routes/blog/[id]/+page.server.js
@@ -1,9 +1,10 @@
 //TODO add load function to get markdown post
-import {files} from "$lib/blog.js";
+import {files, GetFileFromParam} from "$lib/blog.js";
+import {URL} from "$env/static/private";	
 
 export async function load({ params }){
-	const item = files[0];
-	let response = await fetch("https://portfolio-git-dev-andyrobreids-projects.vercel.app/1-Lets_Start.md");
+	const item = GetFileFromParam(param.id);
+	let response = await fetch(URL + "/" + item.path);
 	return {
 		id: item.id,
 		path: item.path,
